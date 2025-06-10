f28aa7358fe5b000cd6e04b5529277f976e13e12
Gabriela <gabiqrz25@gmail.com>
2025-06-10 20:07:08 -0300

    empty-full-stack

--- stack.c
+++ stack.c
@@ -5,21 +5,30 @@
 struct _stack {
     float * values;
     int top;
+    int maxSize;
 };
 
 Stack* newStack(int size){
     Stack* stack = (Stack*) calloc(1, sizeof(struct _stack));
     stack->values = (float*) calloc(size, sizeof(float));
-
     stack->top = 0;
+    stack->maxSize = size;  
     return stack;
 }
 
 void stackPush(Stack* stack, float value){
+    if (stack->top >= stack->maxSize) {
+        printf("Erro: Pilha cheia!\n");
+        return;
+    }
     stack->values[stack->top++] = value;
 }
 
 float stackPop (Stack *stack){
+     if (stack->top == 0) {
+        printf("Erro: Pilha vazia!\n");
+        return 0;
+    }
     return stack->values[--stack->top];
 }
 
@@ -30,5 +39,5 @@ void stackPrint(Stack* stack){
         printf ("\n%0.f\n", stack->values[i]);
         }
     }
-    printf("----------Stack----------\n");
+    printf("-------------------------\n");
 }
