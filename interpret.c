4d13e69dccdf2a6b61254e2438bd95e6bb4371ec
gabrielaqueirxz <gabiqrz25@gmail.com>
2025-06-22 17:10:14 -0300

    upd-interpret

--- interpret.c
+++ interpret.c
@@ -3,8 +3,14 @@
 #include <stdio.h>
 #include "interpret.h"
 #include "stack.h"
+#include "list.h"
 
 Stack *stack;
+List *list;
+
+void generateList () {
+    list = createList();
+}
 
 void generateStack (int quantValues) {
     stack= newStack(quantValues);
@@ -18,19 +24,23 @@ void interpret (const char *input, int print) {
     long val = strtol(arg, &endptr, 10);
 
     if (strcmp(op, "push") == 0) {
-        if (*endptr == '\0') {
-        stackPush(stack, (int)val);
-    } else {
-        printf("Erro: valor inválido para a operação 'push'. Digite apenas um número inteiro.\n");
+        if (sscanf(arg, "%d", &a) == 0 ) {
+        a = listGet (list, arg);
     }
-
+    stackPush(stack, a);
     } else if (strcmp(op, "pop") == 0) {
-        a = stackPop(stack);
-
+        a = stackPop (stack);
+        if (listExist (list, arg)) {
+        listSet(list, arg, a);
+        } else {
+        listAppend(list, arg, a);
+        }
     } else if (strcmp(op, "print") == 0) {
         printf("Resultado: %d\n", stackPop(stack));
-        if (print != 0) stackPrint(stack);
-
+        if (print != 0) {
+            listPrint(list);
+            stackPrint(stack);
+        }
     } else if (
         strcmp(op, "add") == 0 ||
         strcmp(op, "sub") == 0 ||
@@ -55,7 +65,6 @@ void interpret (const char *input, int print) {
                 stackPush(stack, b / a);
             }
         }
-
     } else {
         printf("Erro: comando inválido!\n");
         printf("Comandos: push <valor>, pop, add, sub, mul, div, print, exit.\n");
